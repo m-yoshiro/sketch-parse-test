c9321624de7d0ececcd35801235510c3f09ec9d9
m-yoshiro <massugu.massugu@gmail.com>
2018-05-04 18:49:56 +0900

    convert class to function

--- index.js
+++ index.js
@@ -5,29 +5,23 @@ const { extname } = require('path');
 const loadSketch = require('./lib/load-sketch');
 const Sketch = require('./lib/sketch');
 
-// const SKETCH_CLASS_KEYS = {
-//   layers: ['page', 'artboard', 'shapeGroup', 'rectangle'],
-//   frame: ['rect'],
-//   style: ['style']
-// };
-
-class SketchParser {
-  loadSketch(path) {
-    if (extname(path) !== '.sketch') {
-      throw new Error('File must be a sketch file.');
-    }
-
-    fs.readFile(path, (err, data) => {
-      if (err) {
-        throw err;
-      }
-      loadSketch(data).then(data => {
-        return Sketch.setSketchData(data);
-      });
-    });
-
-    return Sketch;
+const loadFile = function(path) {
+  if (extname(path) !== '.sketch') {
+    throw new Error('File must be a sketch file.');
   }
-}
 
-module.exports = new SketchParser();
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    loadSketch(data).then(data => {
+      return Sketch.setSketchData(data);
+    });
+  });
+
+  return Sketch;
+};
+
+module.exports = {
+  loadFile
+};
