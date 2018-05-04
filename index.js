671f9b30cc7fb63b0a21a93c59d09bfe13f3dd36
m-yoshiro <massugu.massugu@gmail.com>
2018-05-04 17:36:58 +0900

    Move methods

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 const fs = require('fs');
 const { extname } = require('path');
 const loadSketch = require('./lib/load-sketch');
-const { getLayersByClass } = require('./lib/helper');
-const map2Obj = require('./lib/map2Obj');
+const Sketch = require('./lib/sketch');
 
 // const SKETCH_CLASS_KEYS = {
 //   layers: ['page', 'artboard', 'shapeGroup', 'rectangle'],
@@ -20,8 +19,6 @@ class SketchParser {
   }
 
   loadSketch(path) {
-    const parser = this;
-
     if (extname(path) !== '.sketch') {
       throw new Error('File must be a sketch file.');
     }
@@ -30,59 +27,23 @@ class SketchParser {
       if (err) {
         throw err;
       }
-      parser._loadSketch(data);
+      loadSketch(data).then(data => {
+        return Sketch.setSketchData(data);
+      });
     });
-  }
 
-  toString() {
-    return map2Obj(_data.get(this));
-  }
-
-  toJson() {
-    return JSON.stringify(map2Obj(_data.get(this)));
+    return Sketch;
   }
 
   filenames() {
     return _data.get(this).keys();
   }
 
-  getByFilename(filename) {
-    return _data.get(this).get(filename);
-  }
-
-  getDocument() {
-    return this.get('meta.json');
-  }
-
-  getPages() {
-    const pageIds = Object.keys(this.getMetaProperties('pagesAndArtboards'));
-    const _pages = {};
-
-    pageIds.forEach(id => {
-      const key = `pages/${id}.json`;
-      _pages[key] = this.get(key);
-    }, this);
-
-    return _pages;
-  }
-
-  getUser() {
-    return this.get('user.json');
-  }
-
-  getMeta() {
-    return this.get('meta.json');
-  }
-
-  getLayersByClassname() {
-    return getLayersByClass;
-  }
-
   // Private methods
 
   _loadSketch(file) {
     loadSketch(file).then(data => {
-      _data.set(this, data);
+      Sketch.setSketchData(data);
     });
   }
 }
